fa3b60904bf1814cf3613d6f27dda933b44a6095
sktkddn777 <sktkddn777@naver.com>
2021-08-12 01:33:53 +0900

    save HTML object using DOM

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,16 @@
 "use strict";
 
-console.log("login middle ware");
-console.log("hello");
+const id = document.querySelector("#id"),
+    pw = document.querySelector("#pw"),
+    loginBtn = document.querySelector("button");
+
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        pw: pw.value,
+    };
+    console.log(req);
+}
