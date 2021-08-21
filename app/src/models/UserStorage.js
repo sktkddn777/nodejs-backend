1d82dc7051546f16a305e0aa7d3a21d388328c11
sktkddn777 <sktkddn777@naver.com>
2021-08-21 23:01:04 +0900

    file system을 이용한 로그인 수정

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -7,19 +7,16 @@ const fs = require("fs").promises;
 // 변수를 private하게 #
 class UserStorage {
   static #getUserInfo(data, id) {
-    if (err) throw err;
-        const users = JSON.parse(data);
-        const idx = users.id.indexOf(id);
-
-        const userInfo = Object.keys(users).reduce((newUser, info) => {
-          newUser[info] = users[info][idx];
-          return newUser;
-        }, {});
-        return userInfo;
+    const users = JSON.parse(data);
+    const idx = users.id.indexOf(id)
+    const userInfo = Object.keys(users).reduce((newUser, info) => {
+      newUser[info] = users[info][idx];
+      return newUser;
+    }, {});
+    return userInfo;
   }
 
   static getUsers(...fields) {
-    const users = this.#users;
     const newUsers = fields.reduce((newUsers, field) => {
       if (users.hasOwnProperty(field)) {
         newUsers[field] = users[field];
@@ -38,7 +35,6 @@ class UserStorage {
   }
 
   static save(userInfo) {
-    const users = this.#users;
     users.id.push(userInfo.id);
     users.pw.push(userInfo.pw);
     return { success: true };
