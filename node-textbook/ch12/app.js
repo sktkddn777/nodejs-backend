86cab6621361222dacf6868161d0aaad52bcb321
sktkddn777 <sktkddn777@naver.com>
2022-01-30 23:08:26 +0900

    study socketio(not finish yet)

--- node-textbook/ch12/app.js
+++ node-textbook/ch12/app.js
@@ -5,26 +5,23 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
-
+const ColorHash = require('color-hash').default;
 
 dotenv.config();
-const app = express();
 const webSocket = require('./socket');
 const indexRouter = require('./routes');
+const connect = require('./schemas');
 
+const app = express();
 app.set('port', process.env.PORT || 8005);
 app.set('view engine', 'html');
 nunjucks.configure('views', {
   express: app,
   watch: true,
 });
+connect();
 
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
+const sessionMiddleware = session({
   resave: false,
   saveUninitialized: false,
   secret: process.env.COOKIE_SECRET,
@@ -32,7 +29,21 @@ app.use(session({
     httpOnly: true,
     secure: false,
   },
-}));
+});
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(sessionMiddleware);
+
+app.use((req, res, next) => {
+  if (!req.session.color) {
+    const colorHash = new ColorHash();
+    req.session.color = colorHash.hex(req.sessionID);
+  }
+  next();
+});
 
 app.use('/', indexRouter);
 
@@ -53,4 +64,4 @@ const server = app.listen(app.get('port'), () => {
   console.log(app.get('port'), '번 포트에서 대기중');
 });
 
-webSocket(server);
+webSocket(server, app, sessionMiddleware);
--- node-textbook/ch12/socket.js
+++ node-textbook/ch12/socket.js
@@ -1,29 +1,61 @@
-const WebSocket = require('ws');
-const requestIp = require('request-ip');
 
-module.exports = (server) => {
-  const wss = new WebSocket.Server( { server });
+const SocketIO = require('socket.io');
+const axios = require('axios');
+const cookieParser = require('cookie-parser');
 
-  wss.on('connection', (ws, req) => { // FE에서 new로 요청을 보냈을 때 호출
-    const ip = req.headers['x-forwarded-for'] || requestIp.getClientIp(req); // ip파악
-    console.log('새로운 클라이언트 접속', ip);
-    ws.on('message', (message) => { // 클라이언트로 부터 메시지
-      console.log(message);
-    });
-    ws.on('error', (err) => { // 에러
-      console.error(err);
-    });
-    ws.on('close', () => { // 연결 종료
-      console.log('클라이언트 접속 해제', ip);
-      clearInterval(ws.interval);
-    });
+// 계층
+// io > 네임스페이스 > room
+// socket id를 이용하여 1대1 채팅도 가능하다.
 
-    ws.interval = setInterval(() => {
-      if (ws.readyState === ws.OPEN) {
-        ws.send('서버에서 클라이언트로 메시지를 보냅니다.');
-      }
-    }, 3000);
-  }) ;
+module.exports = (server, app, sessionMiddleware) => {
+  const io = SocketIO(server, {path: '/socket.io'}); //socketio server랑 express server랑 연결 시키면 socketio서버가 express에 /socket.io/socket.io.js 준다.
+  app.set('io', io); // req.app.get('io') 처럼 라우터에서 socketio객체를 사용할 수 있다. 
+  const room = io.of('/room'); // 네임스페이스 구분
+  const chat = io.of('/chat');
   
-}
+  const wrap = middleware => (socket, next) => middleware(socket.request, {}, next);
+  chat.use(wrap(cookieParser(process.env.COOKIE_SECRET)));
+  chat.use(wrap(sessionMiddleware));
 
+  room.on('connection', (socket) => {
+    console.log('room 네임스페이스에 접속');
+    socket.on('disconnect', () => { // 연결 종료 시
+      console.log('room 네임스페이스에 접속 해제');
+    });
+  });
+
+  chat.on('connection', (socket) => {
+    console.log('chat 네임스페이스에 접속');
+    const req = socket.request;
+    const { headers: { referer } } = req;
+    console.log('refere', referer);
+
+    const roomId = referer
+      .split('/')[referer.split('/').length - 1]
+      .replace(/\?.+/, '');
+    socket.join(roomId);
+    socket.to(roomId).emit('join', {
+      user: 'system',
+      chat: `${req.session.color}님이 입장하셨습니다.`,
+    })
+
+    socket.on('disconnect', () => { // 연결 종료 시
+      console.log('chat 네임스페이스에 접속 해제');
+      socket.leave(roomId);
+
+      const currentRoom = socket.adapter.rooms[roomId];
+      const userCount = currentRoom ? currentRoom.length : 0;
+
+      if (userCount === 0) {
+
+      } else {
+        socket.to(roomId).emit('exit', {
+          user: 'system',
+          chat: `${req.session.color}님이 퇴장하였습니다.`,
+        });
+      }
+    });
+  });
+};
+
+// socket.emit('news', 'Hello Socket.IO'); // 이벤트 이름, 메시지 (news라는 이벤트에 Hello Socket.IO를 보내라)
